4b57fe3f68f42847132842e294ad17d2f68880ea
santi_arcar <santiago.arxe@estudiantat.upc.edu>
2021-10-06 23:11:57 +0200

    now there's a data cleaner class + heaps plots

--- CAIM-1/delete_noise.py
+++ CAIM-1/delete_noise.py
@@ -1,48 +1,56 @@
 import string
 import os
-
-cwd = os.getcwd()
-
-search_on = 'news'
+import time
 
 
-def has_number(word):
-    for i in range(10):
-        if str(i) in word:
-            return True
-    return False
+class noise_cleaner:
 
+    cwd = os.getcwd()
 
-def has_punctuation(word):
-    for c in word:
-        if c in string.punctuation:
-            return True
-    return False
+    def __init__(self, search_on):
+        self.search_on = search_on
+    @staticmethod
+    def has_number(word):
+        for i in range(10):
+            if str(i) in word:
+                return True
+        return False
 
+    @staticmethod
+    def has_punctuation(word):
+        for c in word:
+            if c in string.punctuation:
+                return True
+        return False
 
-def not_alnum(word):
-    return any(not c.isalnum() for c in word)
+    @staticmethod
+    def not_alnum(word):
+        return any(not c.isalnum() for c in word)
 
+    def clean_noise(self):
+        f = open('{0}_output.txt'.format(self.cwd + '/' + self.search_on), 'r')
+        newOutput = open('{0}_clean.txt'.format(self.cwd + '/' + self.search_on), 'w')
+        deprecatedOutput = open('{0}_deprecated.txt'.format(self.cwd + '/' + self.search_on), 'w')
 
-f = open('{0}_output.txt'.format(cwd + '/' + search_on), 'r')
-newOutput = open('{0}_clean.txt'.format(cwd + '/' + search_on), 'w')
-deprecatedOutput = open('{0}_deprecated.txt'.format(cwd + '/' + search_on), 'w')
+        for linea in f:
+            try:
+                count = linea.split(sep=', ')[0]
+                word = linea.split(sep=', ')[1].strip()
+                if not ((word.isdigit()) or (self.has_number(word)) or (self.has_punctuation(word))
+                        or self.not_alnum(word) or ('ª' in word) or ('º' in word)):
+                    newOutput.write(linea)
+                else:
+                    word = word.replace("_", "")
+                    word = word.split("'", 1)[0]
+                    if not ((word.isdigit()) or (self.has_number(word)) or (self.has_punctuation(word))
+                            or self.not_alnum(word) or ('ª' in word) or ('º' in word)):
+                        newOutput.write(linea)
+                    else:
+                        deprecatedOutput.write(linea)
 
-for linea in f:
-    try:
-        count = linea.split(sep=', ')[0]
-        word = linea.split(sep=', ')[1].strip()
-        if not ((word.isdigit()) or (has_number(word)) or (has_punctuation(word))
-                or not_alnum(word) or ('ª' in word) or ('º' in word)):
-            newOutput.write(linea)
-        else:
-            word = word.replace("_", "")
-            word = word.split("'", 1)[0]
-            if not ((word.isdigit()) or (has_number(word)) or (has_punctuation(word))
-                    or not_alnum(word) or ('ª' in word) or ('º' in word)):
-                newOutput.write(linea)
-            else:
-                deprecatedOutput.write(linea)
+            except Exception as e:
+                break
 
-    except Exception as e:
-        break
+        f.close()
+        newOutput.close()
+        deprecatedOutput.close()
--- CAIM-1/plot_elastic_search_laws.py
+++ CAIM-1/plot_elastic_search_laws.py
@@ -1,18 +1,19 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
 from scipy.optimize import curve_fit
+from delete_noise import noise_cleaner
 
 import warnings
+
+import matplotlib.pyplot as plt
+
 
 warnings.filterwarnings('error')
 
 cwd = os.getcwd()
 
 files_zipf = ['news', 'novels']
-
-files_heap = ["novels" + str(n) for n in range(1, 11)]
 
 
 def func_zipf(x, a, b, c):
@@ -59,19 +60,23 @@ def zipf_checker(files: list):
 
         # FIT ZIPFS
         popt, pcov = curve_fit(func_zipf, xdata, ydata, bounds=(0, [np.inf, np.inf, np.inf]))
-        plt.plot(xdata, func_zipf(xdata, *popt), 'g--', label='fit-with-bounds\nfit: a=%5.3f, b=%5.3f, c=%5.3f\nf = c/(x+b)^a' % tuple(popt))
+        plt.plot(xdata, func_zipf(xdata, *popt), 'g--',
+                 label='fit-with-bounds\nfit: a=%5.3f, b=%5.3f, c=%5.3f\nf = c/(x+b)^a' % tuple(popt))
 
         # FIT A/X
         popt, pcov = curve_fit(func_a_over_x, xdata, ydata, bounds=(0, [np.inf]))
-        plt.plot(xdata, func_a_over_x(xdata, *popt), 'r--', label='fit-with-bounds\nfit: a=%5.3f\nf = a/x' % tuple(popt))
+        plt.plot(xdata, func_a_over_x(xdata, *popt), 'r--',
+                 label='fit-with-bounds\nfit: a=%5.3f\nf = a/x' % tuple(popt))
 
         # FIT LN(X)
         popt, pcov = curve_fit(func_ln, xdata, ydata, bounds=(0, [np.inf, np.inf]))
-        plt.plot(xdata, func_ln(xdata, *popt), 'y--', label='fit-with-bounds\nfit: a=%5.3f, b=%5.3f\nf = -a * ln(x*b)' % tuple(popt))
+        plt.plot(xdata, func_ln(xdata, *popt), 'y--',
+                 label='fit-with-bounds\nfit: a=%5.3f, b=%5.3f\nf = -a * ln(x*b)' % tuple(popt))
 
         # FIT inverse exponential
         popt, pcov = curve_fit(func_invexp, xdata, ydata, bounds=(0, [np.inf, np.inf]))
-        plt.plot(xdata, func_invexp(xdata, *popt), 'k--', label='fit-with-bounds\nfit: a=%5.3f, b=%5.3f\nf = a * e^(b/x)' % tuple(popt))
+        plt.plot(xdata, func_invexp(xdata, *popt), 'k--',
+                 label='fit-with-bounds\nfit: a=%5.3f, b=%5.3f\nf = a * e^(b/x)' % tuple(popt))
 
         # PLOT
 
@@ -80,13 +85,31 @@ def zipf_checker(files: list):
         # plt.yscale('log')
         plt.legend()
         plt.title("ZIPF'S: {0}".format(f))
-        plt.show()
+
+        plt.savefig("zipfs_{0}.png".format(f))
 
         plt.clf()
 
 
+def heaps_checker(file_basic: str, num_of_files: int, index_and_count: bool = False):
+    if index_and_count:
+        for i in range(1, num_of_files + 1):
+            os.system(
+                "python IndexFiles.py --index {1}{0} --path /home/santi_arcar/PycharmProjects/CAIM-LAB/CAIM-1/{1}{0}".format(
+                    str(i), file_basic)
+            )
 
-def heaps_checker(files: list):
+        for i in range(1, num_of_files + 1):
+            os.system("python CountWords.py --index {1}{0} > {1}{0}_output.txt".format(str(i),
+                                                                                       file_basic)
+                      )
+
+    for i in range(1, num_of_files + 1):
+        nc = noise_cleaner("{0}{1}".format(file_basic, str(i)))
+        nc.clean_noise()
+        del nc
+
+    files = ["{0}".format(file_basic) + str(n) for n in range(1, num_of_files + 1)]
 
     data = {}
 
@@ -96,6 +119,16 @@ def heaps_checker(files: list):
         distinct_words = df.shape[0]
 
         data[total_words] = distinct_words
+
+    # x = sorted(data)
+    #
+    # median = x[len(x)//2]
+    #
+    # xdata = []
+    #
+    # for i in x:
+    #     if i < median*2:
+    #         xdata.append(i)
 
     xdata = sorted(data)
 
@@ -112,7 +145,8 @@ def heaps_checker(files: list):
 
     # FIT HEAPS
     popt, pcov = curve_fit(func_heap, xdata, ydata)
-    plt.plot(xdata, func_heap(xdata, *popt), 'go--', label='fit-with-bounds\nfit: k=%5.3f, b=%5.3f\nf = k * x^b' % tuple(popt))
+    plt.plot(xdata, func_heap(xdata, *popt), 'go--',
+             label='fit-with-bounds\nfit: k=%5.3f, b=%5.3f\nf = k * x^b' % tuple(popt))
 
     # # FIT A/X
     # popt, pcov = curve_fit(func_a_over_x, xdata, ydata, bounds=(0, [np.inf]))
@@ -134,13 +168,10 @@ def heaps_checker(files: list):
     plt.ylabel('y')
     # plt.yscale('log')
     plt.legend()
-    plt.title("HEAP'S: {0}".format(f))
-    plt.show()
+    plt.title("HEAP'S: {0}".format(file_basic))
+    plt.savefig("heaps_{0}2.png".format(file_basic))
 
 
 # a = zipf_checker(files=files_zipf)
 
-b = heaps_checker(files=files_heap)
-
-
-
+b = heaps_checker(file_basic='novels', num_of_files=32)
